57543fd42b44c7c109915a43d564f90c4a18f186
Andrey Moshkov <fakesupp@gmail.com>
2019-01-14 18:26:58 +0300

    add brain-even game

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 [![Maintainability](https://api.codeclimate.com/v1/badges/3e1e0beb23d23f9bcfb0/maintainability)](https://codeclimate.com/github/amshkv/project-lvl1-s412/maintainability)
 [![Build Status](https://travis-ci.org/amshkv/project-lvl1-s412.svg?branch=master)](https://travis-ci.org/amshkv/project-lvl1-s412)
+
+## Игра "Brain even"
+
+[![asciicast](https://asciinema.org/a/9815vQTwk2gFyrjRckr9MRd7e.svg)](https://asciinema.org/a/9815vQTwk2gFyrjRckr9MRd7e)
---
+++ src/games/brain-even.js
@@ -0,0 +1,30 @@
+import readlineSync from 'readline-sync';
+
+const getRandomNumber = () => Math.round(Math.random() * 100);
+
+const isEven = number => (number % 2 === 0 ? 'yes' : 'no');
+
+const game = () => {
+  const numberOfCorrectAnswer = 3;
+
+  console.log('Welcome to the Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no".');
+  const username = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${username}!`);
+
+  for (let i = 0; i < numberOfCorrectAnswer; i += 1) {
+    const randomNumber = getRandomNumber();
+    const correctAnsver = isEven(randomNumber);
+    console.log(`Question: ${randomNumber}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (answer !== correctAnsver) {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnsver}'.`);
+      console.log(`Let's try again, ${username}!!`);
+      return;
+    }
+    console.log('Correct!');
+  }
+  console.log(`Congratulations, ${username}!`);
+};
+
+export default game;
