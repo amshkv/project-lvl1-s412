0f04a381fb26e306eb9bb27c08af997149a25efc
Andrey Moshkov <fakesupp@gmail.com>
2019-01-14 11:16:28 +0300

    add eslint, travis, fix devDep (stage 3)

--- src/index.js
+++ src/index.js
@@ -4,5 +4,4 @@ export default () => {
   console.log('Welcome to the Brain Games!');
   const username = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${username}!`);
-  return;
 };
