e64908a1a2cc7e34a09dc76456cc0a12b0f9b766
Andrey Moshkov <fakesupp@gmail.com>
2019-01-15 12:36:17 +0300

    design variables

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -6,9 +6,9 @@ const description = 'Answer "yes" if number even otherwise answer "no".';
 const isEven = number => (number % 2 === 0 ? 'yes' : 'no');
 
 const taskGame = () => {
-  const randomNumber = getRandomNumber();
-  const correctAnswer = isEven(randomNumber);
-  return cons(randomNumber, correctAnswer);
+  const question = getRandomNumber();
+  const correctAnswer = isEven(question);
+  return cons(question, correctAnswer);
 };
 
 export default () => startGame(description, taskGame);
