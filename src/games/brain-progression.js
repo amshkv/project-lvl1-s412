76f938794836aa2199648baa2600173870084be7
Andrey Moshkov <fakesupp@gmail.com>
2019-01-15 20:03:18 +0300

    remove space from first letter

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -13,11 +13,7 @@ import getRandomNumber from '../utilites';
 
 const description = 'What number is missing in the progression?';
 
-const progressionLength = 10;
-
-const getProgression = () => {
-  const progressionStep = getRandomNumber(1, 10);
-  const firstProgressionItem = consToList(getRandomNumber(), l());
+const getProgression = (progressionStep, progressionStartEl, progressionLength) => {
   const iter = (step, acc) => {
     if (step === progressionLength) {
       return reverse(acc);
@@ -25,17 +21,21 @@ const getProgression = () => {
     const item = head(acc) + progressionStep;
     return iter(step + 1, consToList(item, acc));
   };
-  return iter(1, firstProgressionItem);
+  return iter(1, progressionStartEl);
 };
 
 const taskGame = () => {
-  const progression = getProgression();
+  const progressionLength = 10;
+  const progressionStep = getRandomNumber(1, 10);
+  const progressionStartEl = cons(getRandomNumber(), l());
+
+  const progression = getProgression(progressionStep, progressionStartEl, progressionLength);
   const randomElFromProgression = random(progression);
   const question = reduce(
     (item, acc) => (item === randomElFromProgression ? `${acc} ..` : `${acc} ${item}`),
     '',
     progression,
-  );
+  ).trim();
   const answer = listToString(randomElFromProgression);
   return cons(question, answer);
 };
